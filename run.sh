3ce2017d5444c2aaf341ffdcc0f0dbb3086431ea
Rooster <nguyenthoai06011990@gmail.com>
2024-11-02 10:49:55 +0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker build -t roster90/intinitywar-landing-page . 
+docker build -t metafi/intinitywar-landing-page . 
 
 
-docker buildx build --platform linux/amd64,linux/arm64 -t roster90/intinitywar-landing-page:0.1.0 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t metafi/intinitywar-landing-page:0.1.0 --push .
 
 
-docker push roster90/intinitywar-landing-page:0.1.0
+docker push metafi/intinitywar-landing-page:0.1.0
