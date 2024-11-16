9c7acee0f92f072c80e92591568ee309b0af312c
Rooster <nguyenthoai06011990@gmail.com>
2024-11-16 09:29:24 +0700

    Updtae

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker build -t metafi/intinitywar-landing-page . 
+docker build -t  metafi/intinitywar-landing-page:0.1.3 --load. 
 
 
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/intinitywar-landing-page:0.1.1 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t metafi/intinitywar-landing-page:0.1.3 --push .
 
 
-docker push metafi/intinitywar-landing-page:0.1.1
+docker push metafi/intinitywar-landing-page:0.1.3
